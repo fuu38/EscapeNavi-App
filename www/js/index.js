7fd68a5a2b88fae192cd653540cc7da440bacf43
Chaha1n <chahaaaaan@gmail.com>
2020-06-22 16:40:50 +0900

    Add Esacpe-Navi

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,65 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+function setData() {
+    //ボタンが押されたとき実行する
+    var wOptions = {
+        "enableHighAccuracy": true, // true : 高精度
+        "timeout": 10000, // タイムアウト : ミリ秒
+        "maximumAge": 0, // データをキャッシュ時間 : ミリ秒
+    };
+    navigator.geolocation.getCurrentPosition(positionsOK, alert("ブラウザが対応していないか、位置情報の取得が許可されていません。\n 次の画面で許可して下さい。"), wOptions);
+}
+//ボタン押された時の処理
+document.getElementById("getPositionButton").onclick = function() {
+    setData();
+}
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+function getEscapePlaces(lati, long) {
+    //座標をいい感じに変換する
+    const z = 10 //ズームレベル10固定(国土地理院APIの関係)
+        //この辺難しい数学過ぎて正直よくわかってない
+    let L = 85.05112878;
+    let x = Math.floor((long / 180 + 1) * Math.pow(2, (z + 7)));
+    let y = Math.floor((Math.pow(2, (z + 7)) / Math.PI) * (-1 * (Math.atanh(Math.sin(Math.PI * lati / 180))) + Math.atanh(Math.sin(Math.PI * L / 180))));
+    var element = document.getElementById("target");
+    const disaster = element.value;
+    let url = "https://cyberjapandata.gsi.go.jp/xyz/skhb0" + disaster + "/" + z + "/" + Math.floor(x / 256) + "/" + Math.floor(y / 256) + ".geojson";
+    var returnObject = { "siteName": "", "x": 0, "y": 0 };
+    let distance = 99999999999999999999 //どう考えても距離より遠い値入れただけ
+    fetch(url).then(function(responce) {
+        if (responce.StatusCode = "404") {
+            document.getElementById("siteName").innerHTML = "避難できる場所が見つかりませんでした。"
+        }
+        return responce.json();
+    }).then(function(geojson) {
+        geojson.features.forEach(element => {
+            //座標と三平方の定理を使って最短場所を求める
+            //TODO？　候補を1つではなく何個かやる
+            let tmpLat = element.geometry.coordinates[1];
+            let tmpLon = element.geometry.coordinates[0];
+            let tmpSiteName = element.properties.name;
+            let tmpDistance = Math.sqrt(Math.pow(lati - tmpLat, 2) + Math.pow(long - tmpLon, 2));
+            if (distance > tmpDistance) {
+                returnObject.siteName = tmpSiteName;
+                distance = tmpDistance;
+                returnObject.latitude = tmpLat;
+                returnObject.longitude = tmpLon;
+            }
+        })
+        return returnObject;
+    }).then((obj) => {
+        document.getElementById("siteName").innerHTML = obj.siteName;
+        console.log(obj.latitude);
+        console.log(obj.longitude);
+    });
+    //setEscapeRootに座標を渡すので、obj.latitude/obj.longitudeを返す
+}
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function setEscapeRoot(PrLat, PrLon, DnLat, DnLon) {
+    //TODO GM APIを叩いて最短距離を求め、マップに設定
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+function positionsOK(positions) {
+    const presentLatitude = positions.coords.latitude;
+    const presentLongitude = positions.coords.longitude;
+    //placeには名前、座標が必要
+    getEscapePlaces(presentLatitude, presentLongitude);
+}
